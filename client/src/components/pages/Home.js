4c2ba329fa2fbdf36f2f410c1439f8b7699be649
rohanJa <rohan.jain150300@gmail.com>
2020-07-02 21:55:26 +0530

    Login, Register, Navbar and Home added.

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactForm';
+import ContactFilter from '../contacts/ContactFilter';
+
+
+const Home = () => {
+    return (
+        <div className='grid-2'>
+            <div>
+                <ContactForm />         {/* Contact form to add contact */}
+            </div>
+            <div>
+                <ContactFilter />       {/* Filter used to filter user on the basis of name and email*/}
+                <Contacts />            {/* Contact added in the list */}
+            </div>
+        </div>
+    )
+}
+
+export default Home;
